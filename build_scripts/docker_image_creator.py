fd4e87269d3ec87c7de9292da62353c0025f192a
Jack Mazanec <jmazane@amazon.com>
2018-07-10 11:19:29 -0700

    refactored for pytorch

--- build_scripts/docker_image_creator.py
+++ build_scripts/docker_image_creator.py
@@ -1,7 +1,7 @@
-""" Script to create SageMaker PyTorch Docker images
+""" Script to create Sagemaker PyTorch Docker images
 
     Usage:
-        python docker_image_creator.py optimized_binary_link gpu|cpu pytorch_version python_version
+        python docker_image_creator.py binary_path gpu|cpu pytorch_version python_version
 """
 import argparse
 import glob
@@ -10,59 +10,73 @@ import shutil
 import subprocess
 import sys
 
-## GLOBALS
-DOCKER = 'docker' # Stores which type of DOCKER to use, nvidia-docker or docker
-PATH_TO_SCRIPT = ''  # Stores route the user takes to build scripts directory
-
-def create_docker_image(optbin_link, processor, framework_version, python_version):
-    """ Function builds a docker image with the PT optimized binary
+def create_docker_image(binary_path, processor, framework_version, python_version, final_image_repository, final_image_tags):
+    """ Function builds a docker image
 
     Args:
-        optbin_link (str): link to where the optimized binary is
         processor (str): gpu or cpu
         framework_version (str): pytorch version i.e 0.4.0
         python_version (str): (i.e. 3.6.5 or 2.7.4)
+        binary_path (str): path to where the binary is.
+        final_image_repository (str): name of final repo. If None, 'preprod-pytorch' will be used
+        final_image_tags (list(str)): list of tag names for final image. If set to empty, default tag will be used
     """
     # Initialize commonly used variables
     py_v = 'py{}'.format(python_version.split('.')[0]) # i.e. py2
+    base_docker_path = '{}/../docker/{}/base/Dockerfile.{}'.format(PATH_TO_SCRIPT, framework_version, processor)
+    final_docker_path = '{}/../docker/{}/final'.format(PATH_TO_SCRIPT, framework_version)
 
-    # Get optimized binary - and put in final docker image repo
-    print('Getting optimized binary...')
-    optbin_filename = 'pytorch-{}-cp27-cp27mu-manylinux1_x86_64.whl'.format(framework_version)
-    with open('{}/../docker/{}/final/{}'.format(PATH_TO_SCRIPT, framework_version, optbin_filename), 'wb') as optbin_file:
-        subprocess.call(['curl', optbin_link], stdout=optbin_file)
+    # Get binary file
+    print('Getting binary...')
+    binary_filename = binary_path.split('/')[-1]
+    if os.path.isfile(binary_path):
+        shutil.copyfile(binary_path, '{}/{}'.format(final_docker_path, binary_filename))
+    else:
+        with open('{}/{}'.format(final_docker_path, binary_filename), 'wb') as binary_file:
+            subprocess.call(['curl', binary_path], stdout=binary_file)
 
     # Build base image
     print('Building base image...')
     image_name = 'pytorch-base:{}-{}-{}'.format(framework_version, processor,  py_v)
-    base_docker_path = '{}/../docker/{}/base/Dockerfile.{}'.format(PATH_TO_SCRIPT, framework_version, processor)
     subprocess.call([DOCKER, 'build', '-t', image_name, '-f', base_docker_path, '.'])
 
     #  Build final image
     print('Building final image...')
-    subprocess.call(['python', 'setup.py', 'sdist'], cwd='{}/..'.format(PATH_TO_SCRIPT))
-    output_file = glob.glob('{}/../dist/sagemaker_pytorch_container-*.tar.gz'.format(PATH_TO_SCRIPT))[0] # use glob to use regex
+    subprocess.call(['python', 'setup.py', 'bdist_wheel'], cwd='{}/..'.format(PATH_TO_SCRIPT))
+    output_file = glob.glob('{}/../dist/sagemaker_pytorch_container-*.tar.gz'.format(PATH_TO_SCRIPT))[0]
     output_filename = output_file.split('/')[-1]
-    shutil.copyfile(output_file, '{}/../docker/{}/final/{}'.format(PATH_TO_SCRIPT, framework_version, output_filename))
-    subprocess.call([DOCKER, 'build', '-t', 'preprod-pytorch:{}-{}-{}'.format(framework_version, processor, py_v), \
-                    '--build-arg', 'py_version={}'.format(py_v[-1]), '--build-arg', 'framework_installable={}'.format(optbin_filename), \
-                    '-f', 'Dockerfile.{}'.format(processor), '.'], cwd='{}/../docker/{}/final'.format(PATH_TO_SCRIPT, framework_version))
+    shutil.copyfile(output_file, '{}/{}'.format(final_docker_path, output_filename))
+
+    final_image_repository = final_image_repository if final_image_repository else 'preprod-pytorch'
+    final_image_tags = final_image_tags if final_image_tags else ['{}-{}-{}'.format(framework_version, processor, py_v)]
+    command_list = [DOCKER, 'build']
+    for tag in final_image_tags:
+        command_list.append('-t')
+        command_list.append('{}:{}'.format(final_image_repository, tag))
+
+    command_list.extend(['--build-arg', 'py_version={}'.format(py_v[-1]),
+                    '--build-arg', 'framework_installable={}'.format(binary_filename),
+                    '-f', 'Dockerfile.{}'.format(processor), '.'])
+    subprocess.call(command_list, cwd=final_docker_path)
 
 if __name__ == '__main__':
     # Parse command line options
     parser = argparse.ArgumentParser(description='Build Sagemaker PyTorch Docker Images')
-    parser.add_argument('optimized_binary_link', help='link to place with optimized binary')
+    parser.add_argument('binary_path', help='path to the binary')
     parser.add_argument('processor_type', choices=['cpu', 'gpu'], help='gpu if you would like to use GPUs or cpu')
-    parser.add_argument('framework_version', help='PyTorch framework version (i.e. 0.4.0)')
+    parser.add_argument('framework_version', help='PyTorch framework version (i.e. 1.8.0)')
     parser.add_argument('python_version', help='Python version to be used (i.e. 2.7.0)')
-    parser.add_argument('--nvidia-docker', action='store_true', help="Enables nvidia-docker usage over docker usage")
+    parser.add_argument('--nvidia-docker', action='store_true', help='Enables nvidia-docker usage over docker usage')
+    parser.add_argument('--final-image-repository', default=None, help='Name of final repo the image is stored in')
+    parser.add_argument('--final-image-tags', default=[], nargs='+', help='List of tag names for final image')
     args = parser.parse_args()
 
     # Set value for docker
-    DOCKER = 'nvidia-docker' if args.nvidia_docker else DOCKER
+    DOCKER = 'nvidia-docker' if args.nvidia_docker else 'docker'
 
     # Sets PATH_TO_SCRIPT so that command can be run from anywhere
     PATH_TO_SCRIPT = os.path.dirname(os.path.abspath(sys.argv[0]))
 
     # Build image
-    create_docker_image(args.optimized_binary_link, args.processor_type, args.framework_version, args.python_version)
+    create_docker_image(args.binary_path, args.processor_type, args.framework_version, args.python_version,
+                        args.final_image_repository, args.final_image_tags)
